d1d20404d757ee2f49bdb7cc4d0cbdd3f5a38c21
whitebeard4708 <38247356+whitebeard4708@users.noreply.github.com>
2018-07-27 02:05:30 +0800

    Add files via upload

---
+++ Practice - Algo - Warm up - Mini-Max Sum.py
@@ -0,0 +1,16 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the miniMaxSum function below.
+def miniMaxSum(arr):
+    total = sum(arr)
+    print ((total - max(arr)), (total - min(arr)))
+if __name__ == '__main__':
+    arr = list(map(int, input().rstrip().split()))
+
+    miniMaxSum(arr)
