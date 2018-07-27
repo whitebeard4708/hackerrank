cf7d1b428da5fc2facaee79b27edb0a05a8a3275
whitebeard4708 <38247356+whitebeard4708@users.noreply.github.com>
2018-07-27 11:11:32 +0800

    Update Practice - Algo - Warm up - Time Conversion.py

--- Practice - Algo - Warm up - Time Conversion.py
+++ Practice - Algo - Warm up - Time Conversion.py
@@ -9,10 +9,16 @@ import sys
 def timeConversion(s):
 
     # Write your code here.
-    if s[-2:] == "AM": return s[:-2]
+    if (s[:2] != "12" and s[-2:] == "AM") or (s[:2] == "12" and s[-2:] == "PM"): return s[:-2]
+    
+    elif (s[:2] == "12" and s[-2:] == "AM"):
+        newhour = int(s[:2])-12
+        newhour1=  "%02d" %newhour
+        return newhour1 + s[2:-2]
+    
     else:
-        newhour = str(int(s[:2]) + 12)
-        return newhour + s[2:-2]
+        newhour2 = str(int(s[:2]) + 12)
+        return newhour2 + s[2:-2]
 
 if __name__ == '__main__':
     f = open(os.environ['OUTPUT_PATH'], 'w')
